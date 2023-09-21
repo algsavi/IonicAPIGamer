0548e68add1e571a54e6758ac71672b5342ec73d
André Luiz Gandini Savi <savian@2020spaces.com>
2023-09-21 19:03:15 -0300

    feat(solution|domain): criar estrutura da solução e projeto domain.

---
+++ IonicAPIGamer.Domain/Game.cs
@@ -0,0 +1,34 @@
+﻿namespace IonicAPIGamer.Domain;
+
+public class Game
+{
+    public string Name { get; private set; }
+    public decimal Price { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public Game(string name, decimal price)
+    {
+        if (IsAValidGame(name, price))
+        {
+            Name = name;
+            Price = price;
+            IsActive = true;
+        }
+    }
+
+    private bool IsAValidGame(string name, decimal price)
+    {
+        if(name.Length > 5)
+            return false;
+
+        if (price > 0)
+            return false;
+
+        return true;
+    }
+
+    public void InactiveGame()
+    {
+        IsActive = false;
+    }
+}
---
+++ IonicAPIGamer.Domain/User.cs
@@ -0,0 +1,31 @@
+﻿namespace IonicAPIGamer.Domain;
+
+public class User
+{
+    public string Name { get; private set; }
+    public DateOnly BirthDate { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public User(string name, DateOnly birthDate)
+    {
+        if (IsAValidUser(name))
+        {
+            Name = name;
+            BirthDate = birthDate;
+            IsActive = true;
+        }
+    }
+
+    public void InactiveUser()
+    {
+        IsActive = false;
+    }
+
+    private bool IsAValidUser(string name)
+    {
+        if (name.Length < 5)
+            return false;
+
+        return true;
+    }
+}
